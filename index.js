98db4afed4f933ee06e6415712535f5b1a2b2df5
SagarPrasad160 <sagarprasad916@gmail.com>
2023-06-30 16:01:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello from Home" });
+});
+
+app.listen(PORT, () => console.log("Listening at Port 5000"));
